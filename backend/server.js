ca2a1ac0a7a7bfdd39e5b704125b264880521f13
David Tekeste <tekestede@vcu.edu>
2022-07-21 14:46:35 -0400

    done

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,10 @@
+const express = require("express"),
+       app = express(),
+       port = process.env.PORT || 8080,
+       cors = require("cors");
+const bodyParser = require('body-parser');
+const fs = require("fs");
+
 const basicAuth = require("express-basic-auth");
 var { authenticator, upsertUser, cookieAuth } = require("./authentication");
 const auth = basicAuth({
@@ -5,7 +12,6 @@ const auth = basicAuth({
 });
 const cookieParser = require("cookie-parser");
 app.use(cookieParser("82e4e438a0705fabf61f9854e3b575af"));
-
 
 
 app.use(cors({
@@ -17,64 +23,8 @@ app.listen(port, () => console.log("Backend server live on " + port));
 
 app.get("/", (req, res) => {
     res.send({ message: "Connected to Backend server!" });
-});
-
-//add new item to json file
-app.post("/items", addItem)
-
-function addItem(request, response) {
-    // Converting Javascript object (Task Item) to a JSON string
-    let id = request.body.jsonObject.id
-    let task = request.body.jsonObject.task;
-    let curDate = request.body.jsonObject.currentDate;
-    let dueDate = request.body.jsonObject.dueDate;
-    var newTask = {
-      ID: id,
-      Task: task,
-      Current_date: curDate,
-      Due_date: dueDate
-    }
-  
-    var data = fs.readFileSync('database.json');
-    var json = JSON.parse(data);
-    json.push(newTask);
-    fs.writeFile("database.json", JSON.stringify(json), function (err, result) {
-        if (err) {
-            console.log("error", err);
-        } else {
-            console.log("Successfully wrote to file");
-        }
     });
-    response.send(200);
-}
 
-app.get("/items", getItems)
-//** week5, get all items from the json database*/
-  function getItems (request, response) {
-    var data = fs.readFileSync('database.json');
-    
-    //uncomment to see the data being returned 
-    //console.log(JSON.parse(data));
-
-    response.json(JSON.parse(data));
-}
-
-app.get("/items/search", searchItems)
-//**week 5, search items service */
-  function searchItems (request, response) {
-    var searchField = request.query.taskname;
-    //uncomment to see the searchField passed in
-    //console.log(searchField);
-
-    var json = JSON.parse(fs.readFileSync("database.json"));
-    var returnData = json.filter((jsondata) => jsondata.Task.includes(searchField));
-
-    //uncomment to see the todolists found in the backend service//
-    //console.log(returnData);
-    response.json(returnData);
-}
-
-// AUTH---
 app.get("/authenticate", auth, (req, res) => {
     console.log(`user logging in: ${req.auth.user}`);
     res.cookie('user', req.auth.user, { signed: true });
@@ -92,3 +42,55 @@ app.get("/logout", (req, res) => {
     res.clearCookie('user');
     res.end();
 });
+//add new item to json file
+app.post("/items", cookieAuth, addItem)
+
+function addItem (request, response) {
+    // Converting Javascript object (Task Item) to a JSON string
+    let id = request.body.jsonObject.id
+    let task = request.body.jsonObject.task
+    let curDate = request.body.jsonObject.currentDate
+    let dueDate = request.body.jsonObject.dueDate
+    var newTask = {
+      ID: id,
+      Task: task,
+      Current_date: curDate,
+      Due_date: dueDate
+    }
+  
+    var data = fs.readFileSync('database.json');
+    var json = JSON.parse(data);
+    json.push(newTask);
+    fs.writeFile("database.json", JSON.stringify(json), function(err, result) {
+      if (err) { console.log('error', err) }
+      else { console.log('Successfully wrote to file') }
+    });
+    response.send(200)
+    }
+
+app.get("/items", cookieAuth, getItems)
+//** week5, get all items from the json database*/
+  function getItems (request, response) {
+    var data = fs.readFileSync('database.json');
+    
+    //uncomment to see the data being returned 
+    //console.log(JSON.parse(data));
+
+    response.json(JSON.parse(data));
+    // Note this won't work, why? response.send();
+  } 
+
+app.get("/items/search", cookieAuth, searchItems)
+//**week 5, search items service */
+  function searchItems (request, response) {
+    var searchField = request.query.taskname;
+    //uncomment to see the searchField passed in
+    //console.log(searchField);
+
+    var json = JSON.parse (fs.readFileSync('database.json'));
+    var returnData = json.filter(jsondata => jsondata.Task === searchField);
+
+    //uncomment to see the todolists found in the backend service// 
+    //console.log(returnData);
+    response.json(returnData);
+  }
